dc506512a9e835ce2eca1e56c4beeef315e3083c
GrandMaster2004 <121103159+GrandMaster2004@users.noreply.github.com>
2025-06-25 13:32:08 +0530

    done

--- src/app/snippet/[id]/edit/page.tsx
+++ src/app/snippet/[id]/edit/page.tsx
@@ -1,18 +1,24 @@
-import EditsnippetForm from '@/components/ui/EditSnippetForm';
-import React from 'react'
-import { prisma } from "@/lib/prisma"
+import EditsnippetForm from "@/components/ui/EditSnippetForm";
+import React from "react";
+import { prisma } from "@/lib/prisma";
 // import { Button } from '@/components/ui/button';
 // import Link from 'next/link';
-const EditpageSnippet = async ({params}:{params:Promise<{id:string}>}) => {
-  
-    const id = parseInt((await params).id);
-    const snippet = await prisma.snippet.findUnique({
-        where:{
-            id,
-        }
-    })
-    if(!snippet) return <h1>Snippet not found</h1>
-    return <EditsnippetForm snippet = {snippet} />
+
+interface EditPageProps {
+  params: {
+    id: string;
+  };
 }
+
+const EditpageSnippet = async ({ params }: EditPageProps) => {
+  const id = parseInt(params.id);
+  const snippet = await prisma.snippet.findUnique({
+    where: {
+      id,
+    },
+  });
+  if (!snippet) return <h1>Snippet not found</h1>;
+  return <EditsnippetForm snippet={snippet} />;
+};
 
 export default EditpageSnippet;
